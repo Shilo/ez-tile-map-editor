17c8ae3cab64204281d29d19711a10dcdb15ae98
Shilo <shilo86@gmail.com>
2026-05-06 11:59:49 -0700

    chore: consume flyout button as subtree

--- addons/tyle_map_editor/flyout_button/flyout_button_item.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends Resource
-class_name FlyoutButtonItem
-
-@export var title: StringName
-@export var icon: Texture2D
-@export var tooltip := ""
-@export var shortcut: Shortcut
-@export var shortcut_in_tooltip: bool = true
